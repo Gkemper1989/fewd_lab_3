5a0debfade53f40eda1885635309dacdf0c00e15
Gkemper1989 <g.kemperweber@gmail.com>
2020-02-18 21:36:47 +0000

    commint

---
+++ app.js
@@ -0,0 +1,25 @@
+var salutation = function greet() {
+    return 'Haydo!';
+};
+ var salutation = greet();
+
+ function echo(sound) {
+     return 'sound';
+ };
+
+ function greet(name) {
+     return 'Hello ' + name + '!';
+ }
+
+function doSomething(name) {
+    return 'name';
+}
+doSomething('Gustavo');
+
+function multiplyFive(number) {
+    return 9 * 5;
+}
+
+function myFunction() {
+    return 'Hello Word!'
+}
